b2abaf6f086d6c0325a198b8c6ce3dc049d7fb5a
Luca-Ferri <lucaferrip@gmail.com>
2022-07-15 08:08:45 -0300

    Retirando as armadilhas do meu modelo  de lista de negociações

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,26 +1,19 @@
 class ListaNegociacoes {
     #negociacoes;
-    #armadilha;
-    //#contexto;
 
-    constructor(/*contexto,*/ armadilha) {
+    constructor() {
         this.#negociacoes = [];
-        this.#armadilha = armadilha;
-    }
-
-    adiciona(negociacao) {
-        this.#negociacoes.push(negociacao);
-        this.#armadilha(this);
-        //Reflect.apply(this.#armadilha, this.#contexto, [this]);
     }
     
     get negociacoes() {
         return [].concat(this.#negociacoes);
     }
+
+    adiciona(negociacao) {
+        this.#negociacoes.push(negociacao);
+    }
     
     esvazia() {
         this.#negociacoes = [];
-        this.#armadilha(this);
-        //Reflect.apply(this.#armadilha, this.#contexto, [this]);
     }
 }
