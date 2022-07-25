2e8bf586e2c90f89ab0318747e447f96785fddb8
Luca-Ferri <lucaferrip@gmail.com>
2022-07-25 11:54:36 -0300

    Adicionando funcinalidade de ordenar a lista

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,5 +1,6 @@
 class ListaNegociacoes {
     #negociacoes;
+    static #salvaChamada = null;
 
     constructor() {
         this.#negociacoes = [];
@@ -20,4 +21,51 @@ class ListaNegociacoes {
     esvazia() {
         this.#negociacoes = [];
     }
+
+    ordenaPor(criterio) {
+        const valoresDoCriterio = this.#negociacoes.map(negociacao => negociacao[criterio]);
+        const listaOrdenada = [];
+        const loops = this.#negociacoes.length;
+
+        for (let i = 0; i < loops; i++) {
+            const menorValor = this.#encontraMenorValor(valoresDoCriterio);
+            const indexMenorValor = valoresDoCriterio.indexOf(menorValor);
+
+            listaOrdenada.push(this.#negociacoes[indexMenorValor]);
+
+            this.#negociacoes.splice(indexMenorValor, 1);
+            valoresDoCriterio.splice(indexMenorValor, 1);
+        }
+
+        if (this.#inverterLista(criterio)) {
+            listaOrdenada.reverse();
+        }
+
+        this.#negociacoes = listaOrdenada;
+    }
+
+    #encontraMenorValor(valores) {
+        let menorValor = Infinity;
+        valores.forEach(valor => {
+            if (valor < menorValor) menorValor = valor;
+        });
+
+        return menorValor;
+    }
+
+    #inverterLista(criterio) {
+        const chamadaAnterior = ListaNegociacoes.#salvaChamada;
+        ListaNegociacoes.#salvaChamada = criterio;
+        const chamadaAtual = ListaNegociacoes.#salvaChamada;
+
+        return function () {
+            if (chamadaAnterior === chamadaAtual) {
+                ListaNegociacoes.#salvaChamada = null;
+                return true;
+            } else {
+                return false;
+            }
+        }();
+    }
+
 }
