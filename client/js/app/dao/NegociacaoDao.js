5f1ada28b9a4359d46c818e0a854c5ba7b6c3275
Luca-Ferri <lucaferrip@gmail.com>
2022-08-05 09:51:39 -0300

    Adicionando método 'listaTodos' em NegociacaoDao

--- client/js/app/dao/NegociacaoDao.js
+++ client/js/app/dao/NegociacaoDao.js
@@ -31,4 +31,34 @@ class NegociacaoDao {
             };
         });
     }
+
+    listaTodos() {
+        return new Promise((resolve, reject) => {
+            const cursor = this.#connectoin
+            .transaction([this.#store], 'readwrite')
+            .objectStore(this.#store)
+            .openCursor();
+
+            const listaNegociacoes = [];
+
+            cursor.onsuccess = event => {
+                const atual = event.target.result;
+
+                if (atual !== null) {
+                    const dado = atual.value;
+
+                    listaNegociacoes.push(new Negociacao(dado.data, dado.quantidade, dado.valor));
+
+                    atual.continue();
+                } else {
+                    resolve(listaNegociacoes);
+                }
+            };
+
+            cursor.onerror = event => {
+                console.log(event.target.error);
+                reject('Não foi possível listar as negociações.');
+            };
+        })
+    }
 }
