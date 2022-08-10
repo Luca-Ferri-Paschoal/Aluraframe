bc8d6a9d41ee321fac54b8ccf4dfd5c7862e58e8
Luca-Ferri <lucaferrip@gmail.com>
2022-08-09 21:53:52 -0300

    Pronto para usar o sistema de módolos do npm

---
+++ client/js/app/lib/datex.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+var DateHelper = /*#__PURE__*/function () {
+  function DateHelper() {
+    _classCallCheck(this, DateHelper);
+  }
+
+  _createClass(DateHelper, [{
+    key: "dateToString",
+    value: function dateToString(date) {
+      /* faz algo */
+    }
+  }, {
+    key: "stringToDate",
+    value: function stringToDate(string) {
+      /* faz algo */
+    }
+  }]);
+
+  return DateHelper;
+}();
+//# sourceMappingURL=datex.js.map
